b769169a246cea30914b61d6faa87fbbe6c8c2d7
BOB <86759611+DEVPOB@users.noreply.github.com>
2023-01-10 01:56:44 +0700

    Commit

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     [Tooltip("ปรับค่าแรงกระโดด")] public float JumpForce = 5f;
     [Tooltip("ปรับแต่งค่าเช็คความสูงจากตัวPlayerถึงพื้น")] public float GroundRadius = 0.1f;
     [Header("Debug")]
-    [Tooltip("รับค่า x และ y")] [SerializeField] float x;public float y;
+    [Tooltip("รับค่า x และ y")] [SerializeField] float x;
     [Tooltip("เช็คสถานะวิ่งหรือไม่")][SerializeField]bool isRunning;
     [Tooltip("เช็คสถานะยืนอยู่ตรงพื้นหรือไม่")][SerializeField]bool OnGround;
     private void Start() {
@@ -23,15 +23,13 @@ public class PlayerController : MonoBehaviour
         print("ค่าแรงกระโดด : " + JumpForce);
     }
     private void Update() {
+        //เรียกใช้ฟังก์ชั้น
         AnimationManager();
         GroundChecking();
+        TurnFace();
+        // x = รับค่าปุ่มการเคลื่อนที่ในแนวนอน
         x = Input.GetAxis("Horizontal");
-        if(x < 0){
-            transform.rotation = Quaternion.Euler(0f,180f,0f);
-        }else{
-            transform.rotation = Quaternion.Euler(0f,0f,0f);
-
-        }
+        //ถ้ากด LeftShift จะวิ่ง
         if(Input.GetKey(KeyCode.LeftShift)){
             rb.velocity = new Vector2(x * RunningSpeed, rb.velocity.y);
             isRunning = true;
@@ -41,20 +39,42 @@ public class PlayerController : MonoBehaviour
         }
 
 
-
+        //ถ้าแตะพื้นอยู่และกดปุ่ม Space bar จะกระโดด
         if(OnGround == true && Input.GetKeyDown(KeyCode.Space)){
             rb.velocity = new Vector2(rb.velocity.x, JumpForce);
         }
     }
+    //สร้างฟังก์ชั้นชื่อ GroundChecking
     void GroundChecking(){
-        OnGround = Physics2D.OverlapCircle(GroundPos.position, 0.1f, GroundMask);
-        if(OnGround){
-            Anim.SetBool("Jumping", false);
+        /**เช็คว่า Player แตะพื้นหรือไม่ด้วยคำสั่ง Physics2D.OverlapCircle (จุดกึ่งกลางของการเช็ค, รัศมีในการเช็ค, LayerMask)**/
+        OnGround = Physics2D.OverlapCircle(GroundPos.position, GroundRadius, GroundMask);
+    }
+    //สร้างฟังก์ชั้นชื่อ TurnFace
+    void TurnFace(){
+        /** เช็คค่า x โดย x จะรับค่าการเคลื่อนไหวแบบลูกศร หรือ ปุ่ม A / D 
+            หากผู้เล่นกดปุ่ม A หรือ ลูกศรด้านซ้าย ค่า x จะติดลบ แต่กลับกัน
+            หากผู้เล่นกดปุ่ม D หรือ ลูกศรด้านขวา ค่า x จะไม่ติดลบ **/
+        
+        /** จึงสามารถนำข้อสังเกตุข้างต้นมาประยุกต์ได้เป็น
+            ปรับค่าการหมุนด้วยคำสั่ง Quaternion.Euler(float x, float y, float z);
+                            {x = การหมุนในแนวแกน X, y = การหมุนในแนวแกน Y, z = การหมุนในแนวแกน Z} **/
+        if(x < 0){
+            transform.rotation = Quaternion.Euler(0f,180f,0f);
         }else{
-            Anim.SetBool("Jumping", true);
+            transform.rotation = Quaternion.Euler(0f,0f,0f);
         }
 
+        /** แปลคำสั่งข้างต้นได้ว่า 
+            ถ้า(x น้อยกว่า 0){
+                ค่าการหมุนดั้งเดิม เปลื่ยนเป็น หมุน(0, 180, 0);    
+            }นอกเหนือจากนี้{
+                ค่าการหมุน เปลื่ยนเป็น หมุน(0, 0, 0);
+            }**/
+
     }
+
+
+
     void AnimationManager(){
         if(isRunning == true){
             Anim.SetFloat("Walking", 0);
@@ -64,9 +84,29 @@ public class PlayerController : MonoBehaviour
             Anim.SetFloat("Running", 0);
             Anim.SetFloat("Walking", Mathf.Abs(x));
         }
-        
+        /** แปลคำสั่งข้างต้นได้ว่า 
+            ถ้า(กำลังวิ่ง){
+                เล่นอนิเมชั่น เดิน เป็น เท็จ(ไม่เล่นอนิเมชั่นเดิน)    
+                เล่นอนิเมชั่น วิ่ง เป็น จริง(เล่นอนิเมชั่นวิ่ง)    
+            }นอกเหนือจากนี้{
+                เล่นอนิเมชั่น เดิน เป็น จริง(เล่นอนิเมชั่นเดิน)    
+                เล่นอนิเมชั่น วิ่ง เป็น เท็จ(ไม่เล่นอนิเมชั่นวิ่ง)    
+            }**/
+
+        if(OnGround == true){
+            Anim.SetBool("Jumping", false);
+        }else{
+            Anim.SetBool("Jumping", true);
+        }
+        /** แปลคำสั่งข้างต้นได้ว่า 
+            ถ้า(แตะพื้นอยู่จริง){
+                เล่นอนิเมชั่น กระโดด เป็น เท็จ(ไม่เล่นอนิเมชั่นกระโดด)    
+            }นอกเหนือจากนี้{
+                เล่นอนิเมชั่น กระโดด เป็น จริง(เล่นอนิเมชั่นกระโดด)    
+            }**/
 
     }
+    // Debug Zone
     void OnDrawGizmos(){
         Gizmos.DrawWireSphere(GroundPos.position, GroundRadius);
         
